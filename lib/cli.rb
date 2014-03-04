06a51872cb2bdec5d0e56c2fba71b06c464b2a99
jinwen <jinwen@factual.com>
2014-03-04 14:25:47 +0800

    fix param in command line

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,8 +6,8 @@ Usage:
    ninja [command] [options]
 
 [command] are:
-  build [project-name]:    build solr index for a project
-  search [keywords]:       search a ninja
+  build [project-name]:    build solr index for a github project
+  search [keywords]:       search ninjas according to given keywords
   profile [github-id]:     display the ninja profile
 
 [options] are:
@@ -21,14 +21,14 @@ opts = Trollop::with_standard_exception_handling p do
 end
 
 cmd   = ARGV.shift
-param = ARGV.shift
+param = ARGV.join(' ')
 case cmd
 when "build"
   puts "Building solr index for project #{param}"
 when "search"
   puts "Searching people about \"#{param}\""
 when "profile"
-  puts "Getting the profile of \"#{param}\""
+  puts "Getting profile of \"#{param}\""
 else
   p.educate
 end
