be72f3536344d97d0f98bb52e00da93b561e570d
ruichao <ruichao@factual.com>
2014-03-04 23:09:02 +0800

    modified git info script to use github api directly; rm test files

--- git_info.rb
+++ git_info.rb
@@ -1,50 +1,91 @@
 require "json"
-require 'rsolr'
+require "octokit" # github API
+require 'rsolr' # solr API
+
+ACCESS_TOKEN = '5d51d6c008713b6f03999d8f542d1f12e4de1b09'
+
+@client = Octokit::Client.new :access_token => ACCESS_TOKEN
+Octokit.auto_paginate = true
 
 @records = {}
-def inital(contributer)
-  @records[contributer] = {}
-  @records[contributer]['assigned_issues'] = []
-  @records[contributer]['opened_issues'] = []
-  @records[contributer]['commits'] = []
+def inital(contributor)
+  @records[contributor] = {}
+  @records[contributor]['assigned_issues'] = []
+  @records[contributor]['opened_issues'] = []
+  @records[contributor]['commits'] = []
+  @records[contributor]['commit_comments'] = []
+  @records[contributor]['issue_comments'] = []
 end
 
+def getRepoContributors(repo)
+  contributors = @client.contributors_stats repo
+  contributors.each do |contributor|
+    puts contributor.author.login
+  end
+end
 
-fr_issues = File.open('scarecrow_issues.json')
-content = fr_issues.read
-issues = JSON.parse(content);
-puts issues.length
-for issue in issues
-  if issue['assignee']
-    if !@records.has_key?(issue['assignee']['login'])
-      inital(issue['assignee']['login'])
+def getRepoIssues(repo)
+  issues = @client.issues repo
+  issues.each do |issue|
+    if issue.assignee
+      if !@records.has_key?(issue.assignee.login)
+        inital(issue.assignee.login)
+      end
+      @records[issue.assignee.login]['assigned_issues'] << issue.body
     end
-    @records[issue['assignee']['login']]['assigned_issues'] << issue['body']
+    if !@records.has_key?(issue.user.login)
+      inital(issue.user.login)
+    end
+    @records[issue.user.login]['opened_issues'] << issue.body
   end
-  if !@records.has_key?(issue['user']['login'])
-    inital(issue['user']['login'])
-  end
-  @records[issue['user']['login']]['opened_issues'] << issue['body']
 end
 
 
-fr_commits = File.open('scarecrow_commits.json')
-content = fr_commits.read
-commits = JSON.parse(content)
-for commit in commits
-  if !@records.has_key?(commit['committer']['login'])
-    inital(commit['committer']['login'])
+def getRepoIssueComments(repo)
+  comments = @client.issues_comments repo
+  comments.each do |comment|
+    if !@records.has_key?(comment.user.login)
+      inital(comment.user.login)
+    end
+    @records[comment.user.login]['issue_comments'] << comment.body
   end
-  @records[commit['committer']['login']]['commits'] << commit['commit']['message']
 end
 
+def getRepoCommits(repo)
+  commits = @client.commits(repo)
+  commits.each do |commit|
+    if !@records.has_key?(commit.user.login)
+      inital(commit.user.login)
+    end
+    @records[commit.user.login]['commits'] << commit.commit.message
+  end
+end
+
+
+def getRepoCommitComments(repo)
+  comments = @client.commit_comments repo
+  comments.each do |comment|
+    if !@records.has_key?(comment.user.login)
+      inital(comment.user.login)
+    end
+    @records[comment.user.login]['commit_comments'] << comment.body
+  end
+end
+
+repo = 'factual/scarecrow'
+getRepoIssues(repo)
+getRepoCommits(repo)
+getRepoIssueComments(repo)
+getRepoCommitComments(repo)
+
+
+# store into solr
 solr = RSolr.connect :url => 'http://10.20.10.249:8983/solr/ninja'
 @records.each do |id, content|
   content.each do |key, value|
-    content[key] = value.join(' ')
+    content[key] = value.join("\n")
   end
   content['id'] = id
-  puts JSON.generate(content)
   solr.add content
   solr.commit
 end
