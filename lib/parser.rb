0d62b6fb1fb37048318958b68212773083bba727
Fan Chen <fanchen@Fans-MacBook-Pro.local>
2014-03-05 10:08:03 +0800

    Move into Parser

--- lib/parser.rb
+++ lib/parser.rb
@@ -4,22 +4,17 @@ require 'rsolr'
 module Ninja
   class QueryParser
     
-    def convert_to_solr_query(keywords)
-      #TODO
+    def convert_to_solr_query(*keywords)
+      return '*:*' if key_words.empty?
+      
+      result_str = "commits:#{key_words[0]} OR assigned_issues:#{key_words[0]} OR opened_issues:#{key_words[0]}"
+      key_words.shift
+      key_words.each do |word|
+        result_str = result_str + " OR commits:#{word} OR assigned_issues:#{word} OR opened_issues:#{word}"
+      end
+
+      return result_str
     end
   end
 end
 
-def getQueryStr(*key_words)
-
-  return '*:*' if key_words.empty?
-  
-  result_str = "commits:#{key_words[0]} OR assigned_issues:#{key_words[0]} OR opened_issues:#{key_words[0]}"
-  key_words.shift
-  key_words.each{|word|result_str = result_str + " OR commits:#{word} OR assigned_issues:#{word} OR opened_issues:#{word}"}
-
-  return result_str
-
-end
-
-puts getQueryStr('test', 'mul')
