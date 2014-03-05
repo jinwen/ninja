3edcedf7386f7ff85a056648874f8c03f0c29b81
ruichao <ruichao@factual.com>
2014-03-05 13:30:52 +0800

    modified according to the new shcema

--- lib/github.rb
+++ lib/github.rb
@@ -1,105 +1,104 @@
 require "json"
 require "octokit" # github API
-require 'rsolr' # solr API
 
 module Ninja
   class Github
-    
-    def self.get_commits(project)
-      #TODO
+    def initialize(repo)
+      @repo = repo
+      @assigned_issues = 'assigned_issues_'+repo
+      @opened_issues = 'opened_issues_'+repo
+      @issue_comments = 'issue_comments_'+repo
+      @commits = 'commits_'+repo
+      @commit_comments ='commit_comments_'+repo
+      @records = {}
+      @ACCESS_TOKEN = '5d51d6c008713b6f03999d8f542d1f12e4de1b09'
+      @client = Octokit::Client.new :access_token => @ACCESS_TOKEN
+      Octokit.auto_paginate = true
     end
 
-    def self.get_issues(project)
-      #TODO
+    def inital(contributor)
+      @records[contributor] = {}
+      @records[contributor]['name'] = 'null'
+      @records[contributor][@assigned_issues] = []
+      @records[contributor][@opened_issues] = []
+      @records[contributor][@commits] = []
+      @records[contributor][@commit_comments] = []
+      @records[contributor][@issue_comments] = []
     end
-  end
-end
 
-ACCESS_TOKEN = '5d51d6c008713b6f03999d8f542d1f12e4de1b09'
-
-@client = Octokit::Client.new :access_token => ACCESS_TOKEN
-Octokit.auto_paginate = true
-
-@records = {}
-def inital(contributor)
-  @records[contributor] = {}
-  @records[contributor]['assigned_issues'] = []
-  @records[contributor]['opened_issues'] = []
-  @records[contributor]['commits'] = []
-  @records[contributor]['commit_comments'] = []
-  @records[contributor]['issue_comments'] = []
-end
-
-def getRepoContributors(repo)
-  contributors = @client.contributors_stats repo
-  contributors.each do |contributor|
-    puts contributor.author.login
-  end
-end
-
-def getRepoIssues(repo)
-  issues = @client.issues repo
-  issues.each do |issue|
-    if issue.assignee
-      if !@records.has_key?(issue.assignee.login)
-        inital(issue.assignee.login)
+    def getRepoContributors()
+      contributors = @client.contributors_stats @repo
+      contributors.each do |contributor|
+        puts contributor.author.login
       end
-      @records[issue.assignee.login]['assigned_issues'] << issue.body
     end
-    if !@records.has_key?(issue.user.login)
-      inital(issue.user.login)
+
+    def getRepoIssues()
+      issues = @client.issues @repo
+      issues.each do |issue|
+        if issue.assignee
+          if !@records.has_key?(issue.assignee.login)
+            inital(issue.assignee.login)
+          end
+          @records[issue.assignee.login][@assigned_issues] << issue.body
+        end
+        if !@records.has_key?(issue.user.login)
+          inital(issue.user.login)
+        end
+        @records[issue.user.login][@opened_issues] << issue.body
+      end
     end
-    @records[issue.user.login]['opened_issues'] << issue.body
-  end
-end
 
 
-def getRepoIssueComments(repo)
-  comments = @client.issues_comments repo
-  comments.each do |comment|
-    if !@records.has_key?(comment.user.login)
-      inital(comment.user.login)
+    def getRepoIssueComments()
+      comments = @client.issues_comments @repo
+      comments.each do |comment|
+        if !@records.has_key?(comment.user.login)
+          inital(comment.user.login)
+        end
+        @records[comment.user.login][@issue_comments] << comment.body
+      end
     end
-    @records[comment.user.login]['issue_comments'] << comment.body
-  end
-end
 
-def getRepoCommits(repo)
-  commits = @client.commits(repo)
-  commits.each do |commit|
-    if !@records.has_key?(commit.user.login)
-      inital(commit.user.login)
+    def getRepoCommits()
+      commits = @client.commits(@repo)
+      commits.each do |commit|
+        if !@records.has_key?(commit.author.login)
+          inital(commit.author.login)
+        end
+        @records[commit.author.login][@commits] << commit.commit.message
+      end
     end
-    @records[commit.user.login]['commits'] << commit.commit.message
-  end
-end
 
 
-def getRepoCommitComments(repo)
-  comments = @client.commit_comments repo
-  comments.each do |comment|
-    if !@records.has_key?(comment.user.login)
-      inital(comment.user.login)
+    def getRepoCommitComments()
+      comments = @client.list_commit_comments @repo
+      comments.each do |comment|
+        if !@records.has_key?(comment.user.login)
+          inital(comment.user.login)
+        end
+        @records[comment.user.login][@commit_comments] << comment.body
+      end
     end
-    @records[comment.user.login]['commit_comments'] << comment.body
+
+    def getRepoInfo()
+      getRepoIssues()
+      getRepoIssueComments()
+      getRepoCommits()
+      getRepoCommitComments()
+      @records.each do |id, content|
+        [@opened_issues, @assigned_issues, @issue_comments, @commits, @commit_comments].each do |key|
+          content[key+'_i'] = content[key].length
+          content[key] = content[key].join("\n")
+        end
+        content['id'] = id
+      end
+      puts @records
+    end
   end
 end
 
 repo = 'factual/scarecrow'
-getRepoIssues(repo)
-getRepoCommits(repo)
-getRepoIssueComments(repo)
-getRepoCommitComments(repo)
-
-
-# store into solr
-solr = RSolr.connect :url => 'http://10.20.10.249:8983/solr/ninja'
-@records.each do |id, content|
-  content.each do |key, value|
-    content[key] = value.join("\n")
-  end
-  content['id'] = id
-  solr.add content
-  solr.commit
-end
+github = Ninja::Github.new(repo)
+github.getRepoInfo()
 
